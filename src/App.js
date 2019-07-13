9ab6c1eee9909a982917b10a5dd07d5cdab86e2b
paulocsg <pauloc.npi@gmail.com>
2019-07-13 11:37:52 -0300

    reactotron and store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import './config/reactotron';
 
 import GlobalStyle from './styles/global';
 
+import store from './store';
+
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <GlobalStyle />
       <h1>Hello world!</h1>
-    </div>
+    </Provider>
   );
 }
 
